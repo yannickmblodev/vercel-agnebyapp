fb71b27ad850966bb9691b7653ee38a91301f8b4
yannickmblodev <yannickmblodev@gmail.com>
2025-09-16 23:22:42 +0000

    add verceljson 3

--- next.config.js
+++ next.config.js
@@ -1,21 +1,44 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config, { dev }) => {
+//     config.cache = { type: 'memory' };
+//     return config;
+//   },
+//   transpilePackages: ['lucide-react'],
+//   output: 'export',
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
+
+// module.exports = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+// };
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { dev }) => {
+  webpack: (config) => {
     config.cache = { type: 'memory' };
     return config;
   },
   transpilePackages: ['lucide-react'],
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
+  // ❌ PAS d'output: 'export' si tu veux des API/SSR
+
   images: { unoptimized: true },
+
+  eslint: {
+    ignoreDuringBuilds: true, // ne bloque pas le build
+  },
+  typescript: {
+    ignoreBuildErrors: true,  // ne bloque pas le build
+  },
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
